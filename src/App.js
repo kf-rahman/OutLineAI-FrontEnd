0bf639f21f786320f2ae7f1ce9e036eb9a2a5c63
kazirahman <kazif2122r@gmail.com>
2024-08-09 15:53:38 -0400

    setup front end for adding files, need to fix ui

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import $ from "jquery";
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import DragNDrop from "./Components/DragNDrop";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       foo: "bar",
-      resumeData: {}
+      resumeData: {},
+      files:[]
+
     };
 
     ReactGA.initialize("UA-110570651-1");
@@ -35,10 +38,18 @@ class App extends Component {
     this.getResumeData();
   }
 
+  setFiles = (files) => {
+    this.setState({ files }); // Use this.setState to update the files array
+  }
   render() {
     return (
       <div className="App">
         <Header data={this.state.resumeData.main} />
+        <div className='section'>
+    // other jsx
+     <DragNDrop onFilesSelected={this.setFiles} width="300px" height='400px'/>
+    // other jsx
+    </div>
         <Footer data={this.state.resumeData.main} />
       </div>
     );
